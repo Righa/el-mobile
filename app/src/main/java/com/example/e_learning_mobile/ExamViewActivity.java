7092249956377c7c1b2457e2fc92dc882780120a
Righa <54580993+Righa@users.noreply.github.com>
2020-06-14 00:10:01 +0300

    activity sketch

---
+++ app/src/main/java/com/example/e_learning_mobile/ExamViewActivity.java
@@ -0,0 +1,14 @@
+package com.example.e_learning_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExamViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exam_view);
+    }
+}
--- app/src/main/java/com/example/e_learning_mobile/MainActivity.java
+++ app/src/main/java/com/example/e_learning_mobile/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.e_learning_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button login = findViewById(R.id.btn_sign_in);
+        Button register = findViewById(R.id.btn_sign_up);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+            }
+        });
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        });
     }
 }
